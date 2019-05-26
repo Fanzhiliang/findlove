19cbaeb75b03095895e3b78ea14bbd0a0926987d
Fanzhiliang <505911050@qq.com>
2019-05-26 21:47:48 +0800

    国际化
    把页面都国际化了，可能会遗漏部分地方。

--- src/views/userCenter/components/info.vue
+++ src/views/userCenter/components/info.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="info">
     <group label-width="5em">
-      <cell title="个人签名" value="这家伙很懒，什么也没留下..." value-align="left"></cell>
+      <cell :title="$t('signature')" value="这家伙很懒，什么也没留下..." value-align="left"></cell>
       <cell is-link value-align="left" @click.native="isShowCode=true">
-        <span slot="title">二维码</span>
+        <span slot="title">{{$t('QRCode')}}</span>
         <slot><i class="iconfont icon-code"></i></slot>
       </cell>
 
-      <popup-picker title="语言" :data="langList" value-text-align="left" v-model="tempLang" show-name></popup-picker>
+      <popup-picker :title="$t('languageText')" :data="langList" value-text-align="left" v-model="tempLang" show-name :confirm-text="$t('confirm')" :cancel-text="$t('cancel')"></popup-picker>
     </group>
     <XDialog :show.sync="isShowCode" :hide-on-blur="true" @click.native="isShowCode=false">
       <div class="user-row">
@@ -21,50 +21,50 @@
     <div class="icons-row">
       <div class="icon-item">
         <i class="iconfont icon-jibenziliao"></i>
-        <p>帖子44</p>
+        <p>{{$t('post')}}44</p>
       </div>
       <div class="icon-item">
         <i class="iconfont icon-chatfillmtui"></i>
-        <p>消息21</p>
+        <p>{{$t('message')}}21</p>
       </div>
       <div class="icon-item">
         <i class="iconfont icon-group"></i>
-        <p>好友45</p>
+        <p>{{$t('friends')}}45</p>
       </div>
       <div class="icon-item">
         <i class="iconfont icon-image"></i>
-        <p>相册9</p>
+        <p>{{$t('photos')}}9</p>
       </div>
       <div class="icon-item">
         <i class="iconfont icon-footer"></i>
-        <p>浏览123</p>
+        <p>{{$t('browse')}}123</p>
       </div>
       <div class="icon-item">
         <i class="iconfont icon-crown"></i>
-        <p>永久会员</p>
+        <p>{{$t('permanentMember')}}</p>
       </div>
       <div class="icon-item">
         <i :class="'iconfont icon-level-'+userInfo.level"></i>
-        <p>等级</p>
+        <p>{{$t('level')}}</p>
       </div>
     </div>
 
     <div class="two-col-row">
       <div class="col-item">
-        <span class="label">积分</span><span class="value">{{userInfo.integral}}</span>
+        <span class="label">{{$t('integral')}}</span><span class="value">{{userInfo.integral}}</span>
       </div>
       <div class="col-item">
-        <span class="label">签到</span><span class="value">136天</span>
+        <span class="label">{{$t('sign')}}</span><span class="value">136天</span>
       </div>
     </div>
 
-    <group label-width="5em">
-      <cell title="用户ID" :value="userInfo.id"></cell>
-      <cell title="性别" :value="'保密'"></cell>
-      <cell title="生日" :value="'1996-12-25'"></cell>
-      <cell title="在线时间" :value="'42小时'"></cell>
-      <cell title="注册时间" :value="'2019-05-01 23:42'"></cell>
-      <cell title="最后访问" :value="'2019-05-13 13:31'"></cell>
+    <group label-width="6em">
+      <cell :title="$t('userID')" :value="'234324'"></cell>
+      <cell :title="$t('gender')" :value="'保密'"></cell>
+      <cell :title="$t('birthday')" :value="'1996-12-25'"></cell>
+      <cell :title="$t('onlineTime')" :value="'42小时'"></cell>
+      <cell :title="$t('registerTime')" :value="'2019-05-01 23:42'"></cell>
+      <cell :title="$t('lastVisit')" :value="'2019-05-13 13:31'"></cell>
     </group>
   </div>
 </template>
